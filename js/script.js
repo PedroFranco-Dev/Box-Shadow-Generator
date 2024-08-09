84620c629c3eada0aa9173150810cc061c1b59e9
PedroFranco-Dev <pedrofranco.dev1@gmail.com>
2024-08-09 18:35:21 -0300

    Inicial Paramethers

--- js/script.js
+++ js/script.js
@@ -29,13 +29,26 @@ class BoxShadowGenerator {
     }
 
     initialize() {
-
         this.horizontalRef.value = this.horizontal.value
         this.verticalRef.value = this.vertical.value
         this.spreadRef.value = this.spread.value
         this.blurRef.value = this.blur.value
 
+        this.applyRule();
+        this.showRule();
     }
+
+    applyRule() {
+        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`
+        this.currentRule = this.previewBox.style.boxShadow;
+    }
+
+    showRule() {
+        this.rule.innerText = this.currentRule
+        this.webkitRule.innerText = this.currentRule
+        this.mozRule.innerText = this.currentRule
+    }
+
 }
 
 // Elements
