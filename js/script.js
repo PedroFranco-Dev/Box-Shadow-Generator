6c25790868cf0a9bfae5825400369f5badfe45b3
PedroFranco-Dev <pedrofranco.dev1@gmail.com>
2024-08-12 09:28:39 -0300

    Color Function done

--- js/script.js
+++ js/script.js
@@ -43,14 +43,18 @@ class BoxShadowGenerator {
         this.verticalRef.value = this.vertical.value
         this.spreadRef.value = this.spread.value
         this.blurRef.value = this.blur.value
+        this.colorRef.value = this.color.value
 
         this.applyRule();
         this.showRule();
     };
 
     applyRule() {
-        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`
-        this.currentRule = this.previewBox.style.boxShadow;
+        const rgbValue = this.hexToRgb(this.colorRef.value);
+        const shadowRule = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px rgba(${rgbValue})`;
+
+        this.previewBox.style.boxShadow = shadowRule;
+        this.currentRule = shadowRule;
     };
 
     showRule() {
@@ -74,10 +78,19 @@ class BoxShadowGenerator {
             case "blur":
                 this.blurRef.value = value;
                 break
+            case "color":
+                this.colorRef.value = value;
+                break
         }
 
         this.applyRule();
         this.showRule();
+    }
+
+    hexToRgb(hex) {
+        return `${("0x" + hex[1] + hex[2]) | 0}, ${("0x" + hex[3] + hex[4]) | 0}, ${
+            ("0x" + hex[5] + hex[6]) | 0
+          }`;
     }
 }
 
@@ -150,4 +163,12 @@ blur.addEventListener("input", (e) => {
 
     boxShadow.updateValue("blur", value)
 });
+
+color.addEventListener("input", (e) => {
+    const value = e.target.value
+
+    boxShadow.updateValue("color", value)
+});
+
+
 
