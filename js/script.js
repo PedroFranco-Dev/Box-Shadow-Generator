968649f57f21afb2158a05b20429ccae54d93bde
PedroFranco-Dev <pedrofranco.dev1@gmail.com>
2024-08-12 09:50:27 -0300

    Project Finished

--- js/script.js
+++ js/script.js
@@ -103,7 +103,6 @@ class BoxShadowGenerator {
     }
 }
 
-// Elements
 const horizontal = document.querySelector("#horizontal");
 const horizontalRef = document.querySelector("#horizontal-value");
 const vertical = document.querySelector("#vertical");
@@ -112,16 +111,12 @@ const blur = document.querySelector("#blur");
 const blurRef = document.querySelector("#blur-value");
 const spread = document.querySelector("#spread");
 const spreadRef = document.querySelector("#spread-value");
-// New Features
 const color = document.querySelector("#color");
 const colorRef = document.querySelector("#color-value");
 const opacity = document.querySelector("#opacity");
 const opacityRef = document.querySelector("#opacity-value");
 const inset = document.querySelector("#inset");
-
-
 const previewBox = document.querySelector("#box");
-
 const rule = document.querySelector("#rule span");
 const webkitRule = document.querySelector("#webkit-rule span");
 const mozRule = document.querySelector("#moz-rule span");
@@ -147,7 +142,6 @@ const boxShadow = new BoxShadowGenerator(
     );
 
     boxShadow.initialize();
-// Events
 
 horizontal.addEventListener("input", (e) => {
     const value = e.target.value;
@@ -191,8 +185,6 @@ inset.addEventListener("input", (e) => {
     boxShadow.updateValue("inset", value)
 });
 
-// Copy and Past
-
 const rulesArea = document.querySelector("#rules-area");
 const copyInstructions = document.querySelector("#copy-instructions")
 
@@ -208,7 +200,3 @@ rulesArea.addEventListener("click", () => {
     });
 
 });
-
-
-
-
